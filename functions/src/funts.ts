5cb3275fcf5d69bdcc26e7b454dc424f360be130
juleskouam <julkouam@gmail.com>
2023-08-08 17:20:23 +0100

    fred change 08/08

--- functions/src/funts.ts
+++ functions/src/funts.ts
@@ -1,14 +1,22 @@
 import * as admin from "firebase-admin";
 import {getFirestore} from "firebase-admin/firestore";
 //import { info} from "firebase-functions/logger";
+import * as functions from "firebase-functions";
 import { Transfert } from "./types/transfert";
 import { getJpaTransfert } from "./jpa/transfert-jpa";
+import { getApprovalJpa } from "./jpa/approval-jpa";
+import { getJpaUsers } from "./jpa/users-jpa";
 import { Response } from "./types/response";
+import { StatusTranfert } from "./enum/status_enum";
+import { StatusApproval } from "./enum/approval_status";
+import { UserRole } from "./enum/role_enum";
 
 const app=admin.initializeApp({},'appFunct');
 const db = getFirestore(app);
 db.settings({ ignoreUndefinedProperties: true })
 const transfertJPA= getJpaTransfert(db);
+const approvalJPA= getApprovalJpa(db);
+const usersJPA= getJpaUsers(db);
 
 const createTransfert = async (transfert: Transfert) => {
     let transInsert = await transfertJPA.create(transfert);
@@ -28,9 +36,14 @@ const getAllTransfert = async (context:any,isAdmin:boolean) => {
 
 const getOneTransfert = async (transfertId:string) => {
     let transferts = await transfertJPA.getOne(transfertId);
+    let approval = await approvalJPA.findByTransfert(transfertId);
     let res = new Response();
-    res.body = transferts;
     res.message = "Transfert Info";
+    if(transferts.get('status') == StatusTranfert.Open || approval == null){
+        res.body = {...transferts.data(), id: transferts.id}
+    }else{
+        res.body = {...transferts.data(), id: transferts.id, approval:approval}
+    }
     return res;
 }
 
@@ -50,7 +63,29 @@ const updateTransfert = async (transfert: Transfert, transfertId:string) => {
     return res;
 }
 
-export {createTransfert, getAllTransfert, getOneTransfert, deleteTransfert, updateTransfert};
+const updateStatusApproval = async (approvalId:string, status:any, context:any) => {
+    const  statusApp = [StatusApproval.Open, StatusApproval.InApproval, StatusApproval.InProgress, StatusApproval.Approved, StatusApproval.Rejected, StatusApproval.Canceled, StatusApproval.ClosedWon, StatusApproval.Error, StatusApproval.Terminate];
+    let res = new Response();
+    res.message = "Status Approval Updated";
+    if(statusApp.includes(status)){
+        let user = await usersJPA.getOne(context.auth?.uid);
+        if(status == StatusApproval.Terminate && user.role == UserRole.Agent){
+            res.body = await approvalJPA.updateStatus(approvalId, status)
+        }else{
+            throw new functions.https.HttpsError('failed-precondition', 'This Status is not valid for role AGENT');
+        }
+        if((status == StatusApproval.Approved || status == StatusApproval.Rejected) && user.role == UserRole.Client){
+            res.body = await approvalJPA.updateStatus(approvalId, status)
+        }else{
+            throw new functions.https.HttpsError('failed-precondition', 'This Status is not valid for role CLIENT');
+        }
+    }else{
+        throw new functions.https.HttpsError('failed-precondition', 'Status Approval is not valid');
+    }
+    return res;
+}
+
+export {createTransfert, getAllTransfert, getOneTransfert, deleteTransfert, updateTransfert, updateStatusApproval};
 
 
 
---
+++ functions/src/jpa/approval-jpa.ts
@@ -0,0 +1,34 @@
+import { Firestore, FieldPath } from "firebase-admin/firestore";
+//import * as functions from "firebase-functions";
+
+
+const APPROVAL_COLLECTION=process.env.APPROVAL_COLLECTION || 'approvals';
+
+
+class Jpa {
+    public db:Firestore;
+    public constructor(db: Firestore){
+        this.db = db;
+    }
+    
+
+    public async findByTransfert(transfertId:string) {
+        let dataTab:any=[];
+        const snapshot = await this.db.collection(APPROVAL_COLLECTION).where('transfert.id', '==', transfertId).orderBy('startDate', 'desc').get();
+        if (snapshot.empty) {
+            return null;
+        } 
+        snapshot.forEach(doc => {
+            dataTab.push({...doc.data(), id: doc.id});
+        });
+        return dataTab[0];
+    }
+
+    public async updateStatus(approvalId:string, status:string) {
+        const approvalref = this.db.collection(APPROVAL_COLLECTION).doc(approvalId);
+        return await approvalref.set({status:status}, { merge: true });  
+    }
+}
+export function getApprovalJpa(db: Firestore): Jpa {
+    return new Jpa(db);
+};
